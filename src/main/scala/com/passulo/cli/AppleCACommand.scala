3e2ff7fcca1c68900924632368c8f4869f310b1d
Jannik Arndt <jannik@jannikarndt.de>
2022-03-04 12:20:06 +0100

    Implement AppleCA command

--- src/main/scala/com/passulo/cli/AppleCACommand.scala
+++ src/main/scala/com/passulo/cli/AppleCACommand.scala
@@ -1,20 +1,42 @@
 package com.passulo.cli
 
-import com.passulo.StdOutText
-import picocli.CommandLine.Command
+import com.passulo.util.Http
+import com.passulo.{Config, StdOutText}
+import picocli.CommandLine.{Command, Option}
+import pureconfig.ConfigSource
+import pureconfig.generic.auto.*
 
+import java.io.File
 import java.util.concurrent.Callable
 
 @Command(
-  name = "appleCAappleCA",
+  name = "appleCA",
   mixinStandardHelpOptions = true,
-  description = Array("Downloads the Apple WWDR CA G4 CertificateApple WWDR CA G4 Certificate")
+  description = Array("Downloads the Apple WWDR CA G4 CertificateApple WWDR CA G4 Certificate"),
+  usageHelpWidth = 120
 )
 class AppleCACommand extends Callable[Int] {
 
+  @Option(names = Array("-n", "--name"), description = Array("Overrides the name for the file. Default is `AppleWWDRCAG4.cer`"))
+  var name: String = _
+
+  def nameFromConfig: String =
+    ConfigSource.file("passulo.conf").load[Config] match {
+      case Left(_) =>
+        println("passulo.conf not found, using fallback name")
+        "AppleWWDRCAG4.cer"
+      case Right(value) => value.keys.appleCaCert
+    }
+
   def call(): Int = {
-    // TODO
-    println(StdOutText.error("Not implemented yet."))
+
+    val filename = if (name == null || name.isBlank) nameFromConfig else name
+    val fileRef  = new File(filename)
+    val response = Http.download("https://www.apple.com/certificateauthority/AppleWWDRCAG4.cer", fileRef.toPath)
+    response.statusCode() match {
+      case 200  => println(StdOutText.success(s"Successfully loaded certificate to ${fileRef.getAbsolutePath}"))
+      case code => println(StdOutText.error(s"Error from Server (Code $code"))
+    }
     0
   }
 }
--- src/main/scala/com/passulo/util/Http.scala
+++ src/main/scala/com/passulo/util/Http.scala
@@ -6,6 +6,7 @@ import java.net.URI
 import java.net.http.HttpRequest.BodyPublishers
 import java.net.http.HttpResponse.BodyHandlers
 import java.net.http.{HttpClient, HttpRequest, HttpResponse}
+import java.nio.file.Path
 object Http {
 
   val client: HttpClient = HttpClient.newHttpClient
@@ -13,6 +14,11 @@ object Http {
   def get(url: String): HttpResponse[String] = {
     val request = HttpRequest.newBuilder(URI.create(url)).GET().build()
     client.send(request, BodyHandlers.ofString())
+  }
+
+  def download(url: String, path: Path): HttpResponse[Path] = {
+    val request = HttpRequest.newBuilder(URI.create(url)).GET().build()
+    client.send(request, BodyHandlers.ofFile(path))
   }
 
   def post(url: String, body: Json): HttpResponse[String] = {
