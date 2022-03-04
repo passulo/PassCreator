3ca89acb8d2e112932493c932b39a056b9731526
Jannik Arndt <jannik@jannikarndt.de>
2022-03-04 13:34:28 +0100

    Add text to CertificateCommand

--- src/main/scala/com/passulo/cli/CertificateCommand.scala
+++ src/main/scala/com/passulo/cli/CertificateCommand.scala
@@ -12,13 +12,19 @@ import java.util.concurrent.Callable
 )
 class CertificateCommand extends Callable[Int] {
 
-  // noinspection VarCouldBeVal
-  @Option(names = Array("-e", "--e-mail", "--email"), description = Array("Your e-mail-address."), required = true)
+  @Option(names = Array("-e", "--e-mail", "--email"), description = Array("Your e-mail-address."))
   var name: String = _
 
   def call(): Int = {
     // TODO
-    println(StdOutText.error("Not implemented yet."))
+    println(StdOutText.error("This is not implemented yet."))
+    println("Use Keychain to create a certificate signing request.")
+    println("Then create a Pass Identifier in your Apple Developer Account:")
+    println("https://developer.apple.com/account/resources/identifiers/list/passTypeId (beginning with 'pass.')")
+    println("Next, create a new 'Pass Type ID Certificate' at https://developer.apple.com/account/resources/certificates/add")
+    println("Upload the request. Afterwards, download the certificate and import it into your keychain.")
+    println("Then select the certificate and the key and export it as a keystore (`.p12`).")
+    println("Choose a secure password and add it to `passulo.conf`.")
     0
   }
 }
